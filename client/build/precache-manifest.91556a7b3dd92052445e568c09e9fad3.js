eac703117947a13053e5ac7dc6b0c741ac274f65
Ben Graham <benjambo13@gmail.com>
2020-02-15 18:34:31 +0000

    ran npm run build

---
+++ client/build/precache-manifest.91556a7b3dd92052445e568c09e9fad3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1825b3c2d970b77227851b8a44e6d437",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50485f5198cf9e1d49cf",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "50485f5198cf9e1d49cf",
+    "url": "/static/js/2.08c040a2.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.08c040a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4187ad2d26bba7fd52e6",
+    "url": "/static/js/main.c4a7f1dd.chunk.js"
+  },
+  {
+    "revision": "50a1be39b1ad8a962b30",
+    "url": "/static/js/runtime-main.3979187e.js"
+  }
+]);
